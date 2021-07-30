e2838b424707b5004062a589d0d1683efa9040f0
Alyssa D'Souza <alyssa.dsouza63@gmail.com>
2021-07-29 22:52:24 -0400

    Add goals and styling

---
+++ r2ll.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.querySelector("#next").addEventListener("click", () => {
+
+        document.getElementById("front").classList.add("front-next");
+        document.getElementById("left").classList.add("left-next");
+        document.getElementById("middle").classList.add("middle-next");
+        document.getElementById("right").classList.add("right-next");
+
+        setTimeout(function() {
+            var html = document.querySelector("#right").innerHTML;
+            moveContent("#middle", "#right");
+            moveContent("#left", "#middle");
+            moveContent("#front", "#left");
+            document.querySelector("#front").innerHTML = html;
+
+            document.getElementById("front").classList.remove("front-next");
+            document.getElementById("left").classList.remove("left-next");
+            document.getElementById("middle").classList.remove("middle-next");
+            document.getElementById("right").classList.remove("right-next");
+    
+        }, 1000)
+    })
+
+    document.querySelector("#prev-mobile").addEventListener("click", () => {
+        var btn = document.querySelector("#mobile-wheel");
+        var pres = btn.dataset.pres;
+        var prev = btn.dataset.prev;
+        console.log(btn,btn.dataset);
+
+        document.getElementById(pres).classList.add("hide");
+        document.getElementById(prev).classList.remove("hide");
+        document.getElementById(prev).classList.add("fade");
+
+        btn.dataset.pres = prev;
+        btn.dataset.prev = document.getElementById(prev).dataset.prev;
+        btn.dataset.next = pres;
+
+    })
+
+    document.querySelector("#next-mobile").addEventListener("click", () => {
+        var btn = document.querySelector("#mobile-wheel");
+        var pres = btn.dataset.pres;
+        var next = btn.dataset.next;
+
+        document.getElementById(pres).classList.add("hide");
+        document.getElementById(next).classList.remove("hide");
+        document.getElementById(next).classList.add("fade");
+
+        btn.dataset.pres = next;
+        btn.dataset.next = document.getElementById(next).dataset.next;
+        btn.dataset.prev = pres;
+
+    })
+
+})
+
+function moveContent(div1, div2) {
+    document.querySelector(div2).innerHTML = document.querySelector(div1).innerHTML;
+}
