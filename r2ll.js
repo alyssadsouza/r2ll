74f7847fe0e500ebda8ec93f782cda60eed35381
Alyssa D'Souza <alyssa.dsouza63@gmail.com>
2021-07-31 17:55:56 -0400

    Add scroll to top button

--- r2ll.js
+++ r2ll.js
@@ -16,4 +16,24 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector("#exit").classList.add("hidden");
     })
 
+    //Get the button:
+    btn = document.getElementById("back-to-top");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            btn.style.display = "block";
+        } else {
+            btn.style.display = "none";
+        }
+    }
+
 })
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
